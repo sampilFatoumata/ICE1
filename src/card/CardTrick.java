0182877d56262a48e865654e83a691efa18dba9f
Fatoumata Sampil <sampil@sheridancollege.ca>
2024-09-23 19:51:30 -0400

    step four

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,7 +4,7 @@
  */
 package card;
 import java.util.Random;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -29,9 +29,36 @@ public class CardTrick {
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the value of your card (1 to 13): ");
+        int userValue=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the suit of your card (0-3 where 0=Hearts,"
+                + "1=Diamonds,"+ "2=clubs,"+ "3=Spades)");
+        String userSuit=scanner.next();
+        Card userCard=new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(userSuit);
+        
         // and search magicHand here
+        boolean findCard=false;
+        for(Card c : magicHand){
+            if (c.getValue()==userCard.getValue() && c.getSuit().equalsIgnoreCase(userCard.getSuit()))
+            {
+                findCard=true;
+                break;
+            }
+            
+            
+        }
+        if (findCard){
+            System.out.println("Your card is in the magic hand");
+        }
+        else{
+        System.out.println("Your card is not in the magic hand");
         //Then report the result here
         // add one luckcard hard code 2,clubs
+    }
     }
     
 }
